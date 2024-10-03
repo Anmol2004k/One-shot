41e150ad9ba025815047aede9b73c4bc8595c175
Anmol Kumar <at4141anmol@gmail.com>
2024-10-03 18:25:15 +0530

    complet unit 1

--- day1.py
+++ day1.py
@@ -1,33 +1,162 @@
- print("Anmol Kumar", "Cool Bro")
+# print("Anmol Kumar", "Cool Bro")
+# age = 23
+# name = "Anmol"
+# cls = "AIDS"
+# Rollnum = 34
+# Cgpa = 9.3
+# rohit_alive = True
+# Rana_ji = False
+# hmm = None
 
-age = 23
-name = "Anmol"
-cls = "AIDS"
-Rollnum = 34
-Cgpa = 9.3
-rohit_alive = True
-Rana_ji = False
-hmm = None
+# print(Cgpa,name)
+# print(name)
 
-print(Cgpa,name)
-print(name)
+# print("My name is",name)
+# print("I scored in",Cgpa , "my last Year")
 
-print("My name is",name)
-print("I scored in",Cgpa , "my last Year")
+# print(type(age))
+# print(type(Cgpa))
+# print(type(cls))
+# print(type(hmm))
+# print(type(Rana_ji))
+# print(type(rohit_alive))
 
-print(type(age))
-print(type(Cgpa))
-print(type(cls))
-print(type(hmm))
-print(type(Rana_ji))
-print(type(rohit_alive))
+                     # #sum
 
-#sum
+# a = 45
+# b = 6
+# sum = (a+b)
+# print(sum)
+# print("Why is this is happen to me")
+# print("hello")
 
-a = 45
-b = 6
-sum = (a+b)
-print(sum)
-print("Why is thishgappen to me")
-print("Why is this is happen to me")
-print("hello")
+             # #start ONE SHOT 
+            # #input string
+# name = input("Entr your Nmae  ::")
+                 # #integer input 
+# age = int(input(" Whats your Age :"))
+                      # #float input
+# height = float(input("What's your Height :"))
+# print("My name is",name, "I am ", age, "Years old", "My Height is", height,"Cm.")
+
+                           # COnditional statement (IF Elif ELSE)
+# light = input("Enter the color of light >>...")  
+# if(light == "red"):
+#     print("You need to STOP")
+# elif(light == "yellow"):
+#     print("You have to Ready and look around ")
+# elif(light == "green"):
+#     print("You have to goooo..")
+# else:
+#     print("Your light is brooken")
+
+            # single line Conditional statement 
+              # also called terenery operator 
+# food = input("food Choice....?")
+# eat = "yess" if food == "cake" else "NOOO"
+# print(eat)
+          #Without declare variable 
+# food = input("Enter food..?")
+# print("sweet") if food == "cake" or food == "Rasgulla" else print("This is not Sweet")
+
+                       # Clwver if 
+# age = int(input("age ..?"))
+# vote = ("yes","No") [age<= 18]
+# print(vote)
+
+# sal = float(input("Salary..."))
+# tax = sal*(0.1, 0.2)[sal<=500]
+# print(tax)
+
+               #operators
+   #ARTHMATIC OPERATORS 
+# a= 10
+# b = 2
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b)
+# print(a ** b)
+# print(a // b)
+               #RELATIONAL OPERATORS 
+# a = 50
+# b = 20
+
+# print(a==b)
+# print(a!=b)
+# print(a<b)
+# print(a>b)
+# print(a<=b)
+# print(a>=b)
+    #ASSINGMENT OPERATORS
+# num = 10
+# print(num)
+# num = num + 10   # = 20
+# print(num)
+# num += 10
+# print(num)
+# num -= 10
+# print(num)
+# num *= 10
+# print(num)
+# num /= 10
+# print(num)
+# num %= 10
+# print(num)
+# num //= 10
+# print(num)
+                #LOGICAL OOERATORS
+# a = 50
+# b = 30
+# print(not True)
+# print(not False)
+# print(a<b)
+
+#    #and 
+# val1 = True
+# val2 = True
+# val3 = False 
+# val4 = False
+# print("AND Operator",val1 and val2)
+# print("AND Operator",val1 and val3)
+#        #OR
+# print("AND Operator",val1 or  val2)
+# print("AND Operator",val1 or val3)
+# print("AND Operator",val1 or val4)
+
+                #TYPE CONVERSION    OR     TYPE CASTING
+# a =  1
+# b = int('23')
+# sum = (a + b)
+# print(sum)
+# e = float(33444)
+# print(e)
+# r = complex(34)
+# print(r)
+        # Practice Question
+               # QUESTION 1
+# a = int(input("Entear a  1st Number"))
+# b= int(input("Entear a 2nd Number"))
+# sum = (a+b)
+# print("The sum is :",sum)
+
+  # QUESTION 2
+# side = int(input("Enter the side of the SQUARE"))
+# area = side*side
+# print("Area of Given Square is: ",area,"Centimeter")
+
+    # QUESTION 3
+# a = float(input("Entear a  1st Number"))
+# b= float(input("Entear a 2nd Number"))
+# avg =( (a+b)/2)
+# print("Average of two number is :",avg)
+
+    # QUESTION 4
+# a = int(input("Entear a  1st Number"))
+# b= int(input("Entear a 2nd Number"))
+# print(a>=b)
+
+               #chapter2
+         # string and Conditional statement.
