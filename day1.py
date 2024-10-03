dce752f18b97343ba05c30df74729766fd56fc40
Anmol Kumar <at4141anmol@gmail.com>
2024-10-03 14:59:38 +0530

    my commit

---
+++ day1.py
@@ -0,0 +1,33 @@
+ print("Anmol Kumar", "Cool Bro")
+
+age = 23
+name = "Anmol"
+cls = "AIDS"
+Rollnum = 34
+Cgpa = 9.3
+rohit_alive = True
+Rana_ji = False
+hmm = None
+
+print(Cgpa,name)
+print(name)
+
+print("My name is",name)
+print("I scored in",Cgpa , "my last Year")
+
+print(type(age))
+print(type(Cgpa))
+print(type(cls))
+print(type(hmm))
+print(type(Rana_ji))
+print(type(rohit_alive))
+
+#sum
+
+a = 45
+b = 6
+sum = (a+b)
+print(sum)
+print("Why is thishgappen to me")
+print("Why is this is happen to me")
+print("hello")
